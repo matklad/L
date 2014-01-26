ff19b252b60c08bfef60dadd5c08fae923b86e36
Aleksey Kladov <aleksey.kladov@gmail.com>
2014-01-26 14:49:15 +0400

    some pp

--- src/L/Types/AST.hs
+++ src/L/Types/AST.hs
@@ -2,21 +2,62 @@ module L.Types.AST where
 
 data Expression = Variable Var | Constant Int
                 | Operation Op Expression Expression
-                deriving Show
 
 data Var = Var String
-         deriving Show
 
 data Op = Plus | Minus | Multiply | Divide
         | And | Or | Xor
         | Eq | Lt | Gt
-        deriving Show
 
 data Statement = Skip | Seq Statement Statement
                | Assign Var Expression | Read Var | Write Expression
                | If Expression Statement Statement
                | While Expression Statement
-               deriving Show
 
 data Program = Program Statement
-             deriving Show
+
+
+instance Show Var where
+  show (Var x) = x
+
+instance Show Op where
+  show o = case o of
+    Plus -> "+"
+    Minus -> "-"
+    Multiply -> "*"
+    Divide -> "/"
+    And -> "&"
+    Or -> "|"
+    Xor -> "^"
+    Eq -> "=="
+    Lt -> "<"
+    Gt -> ">"
+
+instance Show Expression where
+  show e = case e of
+    (Variable v) -> show v
+    (Constant i) -> show i
+    (Operation o e1 e2) -> "(" ++ show e1 ++ ")" ++  show o
+                           ++ "(" ++ show e2 ++ ")"
+
+instance Show Statement where
+  show s = case s of
+    Skip -> "skip"
+    (Seq s1 s2) -> show s1 ++ ";\n" ++ show s2
+    (Assign v e) -> show v ++ " := " ++ show e
+    (Read v) -> "read(" ++ show v ++ ")"
+    (Write e) -> "write(" ++ show e ++ ")"
+    (If e s1 s2) -> "if (" ++ show e ++ ") {\n"
+                    ++ indent (show s1)
+                    ++ "\n} else {\n"
+                    ++ indent (show s2)
+                    ++ "\n}"
+    (While e s1) -> "while (" ++ show e ++ ") {\n"
+                    ++ indent (show s1)
+                    ++ "\n}"
+
+    where
+      indent = unlines . map ("  " ++) . lines
+
+instance Show Program where
+  show (Program s) = show s ++ "\n"
